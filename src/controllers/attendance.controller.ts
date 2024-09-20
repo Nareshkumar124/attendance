c51b0debe83554e638e6008d7c1bdfd5e137481d
Naresh Kumar <nkk124421@gmail.com>
2024-09-20 09:42:11 +0530

    Attendance api

--- src/controllers/attendance.controller.ts
+++ src/controllers/attendance.controller.ts
@@ -3,6 +3,7 @@ import { ApiResponse } from "../utils/ApiResponse";
 import { asyncHandler } from "../utils/handler";
 import { Request, Response, NextFunction } from "express";
 import { Attendance } from "../models/attendance.model";
+import {AcademicCalendar} from '../models/calendar.model';
 
 const setAttendance = async function (req: Request, todayDate?: Date) {
   let { date, subjectId, studentId } = req.body;
@@ -104,13 +105,77 @@ const unMarkAttendance = asyncHandler(async function (
 
 
 // Month wise attendance controller
-const getAttendanceAccordingToStudent = null;
+const getAttendanceAccordingToStudent = asyncHandler(
+  async function(req:Request,res:Response,next:NextFunction){
 
-const getAttendanceAccordingToSubject = null;
+    const {startDate,endDate,subjectId}=req.body;
 
+    const {_id:studentId}=res.locals.user;
+
+    if(!studentId){
+      throw new ApiError(500,"Internal Server Error")
+    }
+
+    if(!startDate || !endDate){
+      throw new ApiError(
+        400,"Start Date and End is required"
+      )
+    }
+
+    // TODO: Check if subjectId is valid and user have this subjectId or not. 
+
+    if(!subjectId){
+      throw new ApiError(400,"Subject id is required")
+    }
+
+    const attendanceData= AcademicCalendar.aggregate();
+
+    if(!attendanceData){
+      throw new ApiError(500,"Internal Server Error")
+    }
+
+    res.status(200).json(
+      new ApiResponse(200,attendanceData,"Attendance Data")
+    )
+  }
+);
+
+const getAttendanceAccordingToSubject = asyncHandler(
+  async function(req:Request,res:Response,next:NextFunction){
+    
+    const {date,subjectId}=req.body;
+
+    if(!date){
+      throw new ApiError(400,"Date is required")
+    }
+
+    if(!subjectId){
+      throw new ApiError(400,"Subject id is required")
+    }
+
+    const attendanceData= AcademicCalendar.aggregate();
+
+    if(!attendanceData){
+      throw new ApiError(500,"Internal Server Error")
+    }
+
+    res.status(200).json(
+      new ApiResponse(200,attendanceData,"Attendance Data")
+    )
+
+  }
+);
+
+
+const getAttendanceAccordingToCourseOfStudent=null; // Get Attendance of student of all Subjects;
+
+const calculationOfAttendance=null; // Fetch all calculation of attendance of student of all Subjects.
 
 export {
   markAttendance,
   unMarkAttendance,
-  markTodayAttendance
+  markTodayAttendance,
+  calculationOfAttendance,
+  getAttendanceAccordingToStudent,
+  getAttendanceAccordingToSubject
 }
